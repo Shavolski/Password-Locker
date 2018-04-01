55535de9e3fdb220772376e852dc8ee8b32cb0c8
Shavolski <successismyname101@gmail.com>
2018-04-02 01:08:13 +0300

    Added an empty user list

--- user_test.py
+++ user_test.py
@@ -2,3 +2,9 @@ class User:
     """
     Class that generates new instances of contacts
     """
+
+    user_list = []
+
+    def __init__(self,first_name,last_name,):
+        self.first_name = first_name
+        self.last_name = last_name
