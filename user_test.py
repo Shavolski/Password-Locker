07082d2c6e665b006abaf286a43a024f895fabea
Shavolski <successismyname101@gmail.com>
2018-04-02 01:16:26 +0300

    added a method to save the user objects

--- user_test.py
+++ user_test.py
@@ -8,3 +8,11 @@ class User:
     def __init__(self,first_name,last_name,):
         self.first_name = first_name
         self.last_name = last_name
+
+    contact_list = []
+
+    def save_user(self):
+        '''
+        save_user method saves user objects into user_list
+        '''
+        User.user_list.append(self)
