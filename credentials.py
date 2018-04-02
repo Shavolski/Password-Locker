9a401823e4b94f38e01b3dc87af3d6b2f0927b25
Shavolski <successismyname101@gmail.com>
2018-04-02 03:18:05 +0300

    Added methods to the credentials file

--- credentials.py
+++ credentials.py
@@ -2,4 +2,60 @@ class Credentials:
     """
     Class that generates new instances of credentials
     """
-    
+    credential_list = []
+
+    def __init__(self,number,email):
+        self.password = password
+        self.email = email
+
+    credential_list = []
+
+    def save_credential(self):
+        '''
+        save_credential method saves contact objects into credential_list
+        '''
+        Credential.credential_list.append(self)
+    def delete_credential(self):
+        '''
+        delete_credential method deletes a saved credential from the credential_list
+        '''
+        Credential.credential_list.remove(self)
+    @classmethod
+    def find_by_password(cls, password):
+        '''
+        Method that takes in a password and returns a credential that matches that password.
+
+        Args:
+            password: password to search before
+        Returns:
+           Credential of person that matches the password
+        '''
+        for credential in cls.credential_list:
+            if.credential.password == password:
+                return credential
+    @classmethod
+    def credential_exist(cls, password):
+        '''
+        Method that checks if a credential exists from the credential list.
+        Args:
+            password: password to search if it exists
+        Returns :
+            Boolean: True or false depending if the credential exists
+        '''
+        for credential in cls.credential_list:
+            if credential.password == password:
+                    return True
+
+        return False
+
+    @classmethod
+    def display_credential(cls):
+        '''
+        method that returns the credential list
+
+        contact_found = Contact.find_by_number(number)
+        '''
+        return cls.contact_list
+    @classmethod
+    def copy_email(cls, password):
+        pyperclip.copy(credential_found.email)
