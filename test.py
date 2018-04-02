35fcf9fabe48e1810de0d7b33c8cded2b23b4118
Shavolski <successismyname101@gmail.com>
2018-04-02 04:17:56 +0300

    added a class called TestUser

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
 import unittest
 from user import User
 from credentials import Credentials
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in reating test cases.
+    '''
